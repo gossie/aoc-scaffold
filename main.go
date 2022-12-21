ed52337bbb06ddb811bf03e30852b1ee83357e5f
André Schreck <moin@andreschreck.de>
2022-12-21 15:11:39 +0100

    change adventofcode.go when new day is created

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -9,13 +10,21 @@ func main() {
 	command := os.Args[1]
 	switch command {
 	case "create":
-		createDirectory(os.Args[2])
+		if len(os.Args) == 3 {
+			createDirectory(os.Args[2])
+		} else {
+			println("I will help you later, I promise")
+		}
 	case "init":
-		initializeYear(os.Args[2])
+		if len(os.Args) == 3 {
+			initializeYear(os.Args[2])
+		} else {
+			println("I will help you later, I promise")
+		}
 	case "--help":
-		println("I will help you later, I promise")
+		printHelp()
 	default:
-		panic("unknown command: " + command)
+		printHelp()
 	}
 }
 
@@ -27,7 +36,7 @@ func initializeYear(year string) {
 	}
 
 	createFile(fmt.Sprintf("%v/go.mod", directoryName), fmt.Sprintf("module github.com/gossie/adventofcode%v\n\ngo 1.19\n", year))
-	createFile(fmt.Sprintf("%v/adventofcode.go", directoryName), "package main\n\nfunc main() {\n\n}\n")
+	createFile(fmt.Sprintf("%v/adventofcode.go", directoryName), "package main\n\nimport (\n    \"fmt\"\n    \"time\"\n)\n\nfunc main() {\n\n}\n")
 
 	fmt.Println("Created new project for the advent of code", year)
 }
@@ -43,7 +52,76 @@ func createDirectory(name string) {
 	createFile(fmt.Sprintf("%v/%v.txt", name, name), "")
 	createFile(fmt.Sprintf("%v/%v_test.txt", name, name), "")
 
+	mainFileContent := readMainFileContent()
+	cutMainFileContent := append(make([]string, 0, len(mainFileContent)), mainFileContent[:len(mainFileContent)-2]...)
+	cutMainFileContent = append(cutMainFileContent, "\n")
+	cutMainFileContent = append(cutMainFileContent, fmt.Sprintf("    fmt.Println(\"\\nPerforming tasks of %v\")", name))
+	cutMainFileContent = append(cutMainFileContent, fmt.Sprintf("    start%vPart1 := time.Now().UnixMilli()", name))
+	cutMainFileContent = append(cutMainFileContent, fmt.Sprintf("    %vPart1 := %v.Part1(\"%v/%v_test.txt\")", name, name, name, name))
+	cutMainFileContent = append(cutMainFileContent, fmt.Sprintf("    fmt.Println(\"%v, task 1: \", %vPart1, \", took\", (time.Now().UnixMilli() - start%vPart1), \"ms\")", name, name, name))
+	cutMainFileContent = append(cutMainFileContent, "\n")
+	cutMainFileContent = append(cutMainFileContent, fmt.Sprintf("    fmt.Println(\"\\nPerforming tasks of %v\")", name))
+	cutMainFileContent = append(cutMainFileContent, fmt.Sprintf("    start%vPart2 := time.Now().UnixMilli()", name))
+	cutMainFileContent = append(cutMainFileContent, fmt.Sprintf("    %vPart2 := %v.Part2(\"%v/%v_test.txt\")", name, name, name, name))
+	cutMainFileContent = append(cutMainFileContent, fmt.Sprintf("    fmt.Println(\"%v, task 2: \", %vPart2, \", took\", (time.Now().UnixMilli() - start%vPart2), \"ms\")", name, name, name))
+	cutMainFileContent = append(cutMainFileContent, "}")
+	cutMainFileContent = append(cutMainFileContent, "\n")
+
+	cutMainFileContent = ensureImport(cutMainFileContent, name)
+
+	writeMainFileContent(cutMainFileContent)
+
 	fmt.Println("Created files", name)
+}
+
+func readMainFileContent() []string {
+	file, err := os.Open("adventofcode.go")
+	if err != nil {
+		panic("failed opening file")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	lines := make([]string, 0)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+func ensureImport(cutMainFileContent []string, name string) []string {
+	foundImportStart := false
+	var endOfImport int
+	for i, line := range cutMainFileContent {
+		if line == "import (" {
+			foundImportStart = true
+		}
+		if foundImportStart {
+			if line == ")" {
+				endOfImport = i
+			}
+		}
+	}
+	return append(append(append(make([]string, 0, len(cutMainFileContent)+1), cutMainFileContent[0:endOfImport]...), fmt.Sprintf("    \"github.com/gossie/adventofcode2023/%v\"", name)), cutMainFileContent[endOfImport:]...)
+}
+
+func writeMainFileContent(lines []string) {
+	file, err := os.OpenFile("adventofcode.go", os.O_RDWR, 0)
+	if err != nil {
+		panic("failed opening file")
+	}
+	defer file.Close()
+
+	for _, line := range lines {
+		_, err := file.WriteString(line + "\n")
+		if err != nil {
+			panic("failed writing into file: " + err.Error())
+		}
+	}
+	file.Sync()
 }
 
 func createFile(name, content string) {
@@ -59,4 +137,8 @@ func createFile(name, content string) {
 	testInput.Sync()
 
 	defer testInput.Close()
+}
+
+func printHelp() {
+	println("I will help you later, I promise")
 }
